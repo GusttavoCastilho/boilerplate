0f6722d594465f8a21c2522398579fff2b2f8bd1
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-05-10 20:33:08 -0300

    firts commit

---
+++ src/components/Checkbox/styles.ts
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components'
+import { CheckboxProps } from '.'
+
+type LabelProps = Pick<CheckboxProps, 'labelColor'>
+
+export const Wrapper = styled.main``
+
+export const Label = styled.label<LabelProps>`
+  ${({ theme, labelColor }) => css`
+    color: ${theme.colors[labelColor!]};
+  `}
+`
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,77 @@
-import { createGlobalStyle } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  GlobalStyleComponent,
+  DefaultTheme
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: url('/fonts/poppins-v15-latin-300.eot');
+  src: local(''),
+       url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('/fonts/poppins-v15-latin-regular.eot');
+  src: local(''),
+       url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url('/fonts/poppins-v15-latin-600.eot');
+  src: local(''),
+       url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+}
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme, removeBg }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      background-color: ${!removeBg &&
+      css`
+        ${theme.colors.mainBg};
+      `};
+    }
+  `}
 
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
 `
 
 export default GlobalStyles
